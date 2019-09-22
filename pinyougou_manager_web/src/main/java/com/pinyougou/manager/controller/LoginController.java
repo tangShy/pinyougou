809ec6587baef231a4be573f6058826ba1759de7
TangShy <975751290@qq.com>
2019-09-22 14:50:15 +0800

    运营商系统登录与安全控制

---
+++ pinyougou_manager_web/src/main/java/com/pinyougou/manager/controller/LoginController.java
@@ -0,0 +1,25 @@
+package com.pinyougou.manager.controller;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/login")
+@SuppressWarnings("all")
+public class LoginController {
+
+    @RequestMapping("/name")
+    public Map showName() {
+        //1.从spring_secruity中得到用户名
+        String name = SecurityContextHolder.getContext().getAuthentication().getName();
+        //2.将得到的用户名放入到map集合中
+        Map map = new HashMap<>();
+        map.put("loginName", name);
+        return map;
+    }
+
+}
