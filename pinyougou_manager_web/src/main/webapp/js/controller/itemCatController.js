f19917763d7ac7156645664b6b82a972214526fc
TangShy <975751290@qq.com>
2019-09-26 22:48:00 +0800

    添加商品分类增删改功能

--- pinyougou_manager_web/src/main/webapp/js/controller/itemCatController.js
+++ pinyougou_manager_web/src/main/webapp/js/controller/itemCatController.js
@@ -2,6 +2,8 @@
 app.controller('itemCatController' ,function($scope,$controller   ,itemCatService){	
 	
 	$controller('baseController',{$scope:$scope});//继承
+
+    $scope.parentId=0;//上级 ID
 	
     //读取列表数据绑定到表单中  
 	$scope.findAll=function(){
@@ -35,15 +37,17 @@ app.controller('itemCatController' ,function($scope,$controller   ,itemCatServic
 	$scope.save=function(){				
 		var serviceObject;//服务层对象  				
 		if($scope.entity.id!=null){//如果有ID
-			serviceObject=itemCatService.update( $scope.entity ); //修改  
+			serviceObject=itemCatService.update( $scope.entity ); //修改
 		}else{
-			serviceObject=itemCatService.add( $scope.entity  );//增加 
+            $scope.entity.parentId=$scope.parentId;//赋予上级 ID
+			serviceObject=itemCatService.add( $scope.entity  );//增加
 		}				
 		serviceObject.success(
 			function(response){
 				if(response.success){
 					//重新查询 
-		        	$scope.reloadList();//重新加载
+		        	// $scope.reloadList();//重新加载
+                    $scope.findByParentId($scope.parentId);//重新加载
 				}else{
 					alert(response.message);
 				}
@@ -77,7 +81,9 @@ app.controller('itemCatController' ,function($scope,$controller   ,itemCatServic
 		);
 	}
 
+    //根据上级 ID 显示下级列表
     $scope.findByParentId = function (parentId) {
+        $scope.parentId=parentId;//记住上级Id
         itemCatService.findByParentId(parentId).success(
             function (response) {
                 $scope.list = response;
