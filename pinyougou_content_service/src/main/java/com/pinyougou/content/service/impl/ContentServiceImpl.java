95f94bb2cd673a7bb119509e1ea5eca3092f09a1
TangShy <975751290@qq.com>
2019-10-21 22:26:36 +0800

    广告管理之图片轮播

--- pinyougou_content_service/src/main/java/com/pinyougou/content/service/impl/ContentServiceImpl.java
+++ pinyougou_content_service/src/main/java/com/pinyougou/content/service/impl/ContentServiceImpl.java
@@ -106,5 +106,15 @@ public class ContentServiceImpl implements ContentService {
 		Page<TbContent> page= (Page<TbContent>)contentMapper.selectByExample(example);		
 		return new PageResult(page.getTotal(), page.getResult());
 	}
-	
+
+	@Override
+	public List<TbContent> findByCategoryId(Long categoryId) {
+		//根据广告分类ID查询广告列表
+		TbContentExample contentExample = new TbContentExample();
+		Criteria criteria = contentExample.createCriteria();
+		criteria.andCategoryIdEqualTo(categoryId);		//类目id
+		criteria.andStatusEqualTo("1");			//有效状态
+		contentExample.setOrderByClause("sort_order");	//排序
+		return contentMapper.selectByExample(contentExample);
+	}
 }
