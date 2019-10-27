8dbf43e76196f9ffd781c8daeac2db9fada69ca2
TangShy <975751290@qq.com>
2019-10-27 23:46:13 +0800

    关键字搜索

---
+++ pinyougou_search_interface/src/main/java/com/pinyougou/search/service/ItemSearchService.java
@@ -0,0 +1,12 @@
+package com.pinyougou.search.service;
+
+import java.util.Map;
+
+public interface ItemSearchService {
+    /**
+     * 搜索
+     * @param searchMap
+     * @return
+     */
+    Map<String, Object> search(Map searchMap);
+}
---
+++ pinyougou_search_web/src/main/java/com/pinyougou/search/controller/ItemSearchController.java
@@ -0,0 +1,24 @@
+package com.pinyougou.search.controller;
+
+import com.pinyougou.search.service.ItemSearchService;
+import jdk.nashorn.internal.ir.annotations.Reference;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/itemsearch")
+public class ItemSearchController {
+    @Reference
+    private ItemSearchService itemSearchService;
+
+    @RequestMapping("/search")
+    public Map<String, Object> search(@RequestBody Map searchMap) {
+        return itemSearchService.search(searchMap);
+    }
+
+}
+
+
