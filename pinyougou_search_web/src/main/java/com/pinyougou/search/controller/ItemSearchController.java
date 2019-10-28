81bc3371a50f335a993ea8efe2b2f985efb7f2be
TangShy <975751290@qq.com>
2019-10-28 22:29:08 +0800

    fix bug

--- pinyougou_search_web/src/main/java/com/pinyougou/search/controller/ItemSearchController.java
+++ pinyougou_search_web/src/main/java/com/pinyougou/search/controller/ItemSearchController.java
@@ -1,7 +1,7 @@
 package com.pinyougou.search.controller;
 
+import com.alibaba.dubbo.config.annotation.Reference;
 import com.pinyougou.search.service.ItemSearchService;
-import jdk.nashorn.internal.ir.annotations.Reference;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
