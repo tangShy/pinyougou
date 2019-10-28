5ced3272c2c52381658d06a0cfae9b50f89ec6ca
TangShy <975751290@qq.com>
2019-10-28 23:42:42 +0800

    高亮显示

--- pinyougou_search_service/src/main/java/com/pinyougou/search/service/impl/ItemSearchServiceImpl.java
+++ pinyougou_search_service/src/main/java/com/pinyougou/search/service/impl/ItemSearchServiceImpl.java
@@ -5,9 +5,9 @@ import com.pinyougou.pojo.TbItem;
 import com.pinyougou.search.service.ItemSearchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.solr.core.SolrTemplate;
-import org.springframework.data.solr.core.query.Criteria;
-import org.springframework.data.solr.core.query.Query;
-import org.springframework.data.solr.core.query.SimpleQuery;
+import org.springframework.data.solr.core.query.*;
+import org.springframework.data.solr.core.query.result.HighlightEntry;
+import org.springframework.data.solr.core.query.result.HighlightPage;
 import org.springframework.data.solr.core.query.result.ScoredPage;
 
 import java.util.HashMap;
@@ -21,13 +21,41 @@ public class ItemSearchServiceImpl implements ItemSearchService {
     @Override
     public Map<String, Object> search(Map searchMap) {
         Map<String, Object> map = new HashMap<>();
-        Query query = new SimpleQuery("*:*");
-        //添加查询条件
-        Criteria criteria = new Criteria("item_keywords").is(searchMap.get("keywords"));
-        query.addCriteria(criteria);
+//        Query query = new SimpleQuery("*:*");
+//        //添加查询条件
+//        Criteria criteria = new Criteria("item_keywords").is(searchMap.get("keywords"));
+//        query.addCriteria(criteria);
+//
+//        ScoredPage<TbItem> page = solrTemplate.queryForPage(query, TbItem.class);
+//        map.put("rows", page.getContent());
+        //1.查询列表
+        map.putAll(searchList(searchMap));
+        return map;
+    }
 
-        ScoredPage<TbItem> page = solrTemplate.queryForPage(query, TbItem.class);
-        map.put("rows", page.getContent());
+    /**
+     * 根据关键字搜索列表
+     * @param searchMap
+     * @return
+     */
+    private Map<String,Object> searchList(Map searchMap){
+        Map<String,Object> map = new HashMap<>();
+        HighlightQuery highlightQuery = new SimpleHighlightQuery();
+        HighlightOptions highlightOptions = new HighlightOptions().addField("item_title");//设置高亮的域
+        highlightOptions.setSimplePrefix("<em style='color:red'>");//高亮前缀
+        highlightOptions.setSimplePostfix("</em>");//高亮后缀
+        highlightQuery.setHighlightOptions(highlightOptions);//设置高亮选项
+        //按照关键字查询
+        Criteria criteria = new Criteria("item_keywords").is(searchMap.get("keywords"));
+        highlightQuery.addCriteria(criteria);
+        HighlightPage<TbItem> page = solrTemplate.queryForHighlightPage(highlightQuery, TbItem.class);
+        for(HighlightEntry<TbItem> h : page.getHighlighted()) {
+            TbItem item = h.getEntity(); // 获取原实体类
+            if(h.getHighlights().size() > 0 && h.getHighlights().get(0).getSnipplets().size()>0) {
+                item.setTitle(h.getHighlights().get(0).getSnipplets().get(0));//设置高亮的结果
+            }
+        }
+        map.put("rows",page.getContent());
         return map;
     }
 }
--- pinyougou_search_web/src/main/webapp/js/base.js
+++ pinyougou_search_web/src/main/webapp/js/base.js
@@ -1,2 +1,8 @@
 //定义不带分页的模块
-var app=angular.module('pinyougou',[]);
+var app = angular.module('pinyougou', []);
+/*$sce服务写成过滤器*/
+app.filter('trustHtml', ['$sce', function ($sce) {
+    return function (data) {//传入参数时被过滤的内容
+        return $sce.trustAsHtml(data);//返回的是过滤后的内容
+    };
+}]);
